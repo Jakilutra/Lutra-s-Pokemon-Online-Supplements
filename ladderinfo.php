3370523405667438779d974c18ed624139b3e4ff
Jakilutra <jakilutra@hotmail.co.uk>
2012-02-17 01:31:28 +0000

    Improved code.

--- ladderinfo.php
+++ ladderinfo.php
@@ -1,14 +1,11 @@
 <?php
 $tiername = $_GET["tier"];
-$ladderfile = file_get_contents("tier_" . $tiername . ".txt");
-$ladderfile = explode ("%-", $ladderfile);
+$ladderfile = file("tier_" . $tiername . ".txt");
 $ladderinfo = array();
 $ratings = array();
 $rankings = array();
+$standings = array();
 foreach ($ladderfile as $player){
-	if ($player != $ladderfile[0]){
-		$player = substr($player, 7);
-	}
 	$player = explode ("%", $player);
 	if (in_array($player[3], $ratings) == false){
 		array_push($ratings, $player[3]);
@@ -27,8 +24,7 @@ foreach ($ratings as $rating){
 }
 $rankings = implode("", $rankings);
 $rankings = explode("<tr align='left'>", $rankings);
-$rankingscount = count($rankings)-2;
-$standings = array();
+$rankingscount = count($rankings)-1;
 for ($i=1;$i <$rankingscount+1;$i++){
 	array_push($standings, "<td>{$i}</td>" . $rankings[$i]);
 }
