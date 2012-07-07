57a498f43a06d0cab9f22908ce7fcd5a031a5145
unknown <Lutra@Lutra-HP.(none)>
2012-07-07 03:46:26 +0100

    New Server Script

---
+++ script_auth.js
@@ -0,0 +1,49 @@
+auth = {};
+set(construction.source, "authoptions", "auth", "options");
+set(construction.source, "authmembers", "auth", "members");
+auth.echo = function(group, text, channel){
+	var display = auth.options === undefined ? "<timestamp/><b>" + text + "</b>" : "<timestamp/><table width='100%' style='background-color:qlineargradient(x1:0, y1:0, x2:0, y2:1, stop:0.1 " + auth.options[group].minorcolor + ", stop:0.5 " + auth.options[group].majorcolor + "); color:" + auth.options[group].textcolor + ";'><tr><td><center><b><big>" + text + "</big></b></center></td></tr></table>";
+	if (channel > -1){
+		sys.sendHtmlAll(display, channel);
+	}
+	else {
+		sys.sendHtmlAll(display);
+	}
+}
+auth.pm = function(group, text, from, to, recipients, channel){
+	var display = auth.options === undefined ? "<timestamp/> from " + from + " to" + to + " Sent to: " + String(recipients).replace(/,/gi, ", ") + "<br/><b>" + text + "</b>" : "<timestamp/><table width='100%' style='background-color:qlineargradient(x1:0, y1:0, x2:0, y2:1, stop:0.1 " + auth.options[group].minorcolor + ", stop:0.5 " + auth.options[group].majorcolor + "); color:" + auth.options[group].textcolor + ";'><tr><th>Personal Message from <i>" + from + "</i> to <i>" + to + "</i>&nbsp;&nbsp;&nbsp;Sent to: <i>" + String(recipients).replace(/,/gi, ", ") + "</i></th></tr><tr><td><center><b><big>" + text + "</big></b></center></td></tr></table>";
+	var index;
+	if (channel > -1){
+		for (index in recipients){
+			sys.sendHtmlMessage(sys.id(recipients[index]), display, channel);
+		}
+	}
+	else {
+		for (index in recipients){
+			sys.sendHtmlMessage(sys.id(recipients[index]), display);
+		}
+	}
+}
+auth.commands = {
+	echo: function(src, channel, command){
+		if (sys.auth(src) < 3){
+			commanderror(src, "Sorry, you do not have permission to use the echo command (owner command).", channel);
+			return;
+		}
+		if (command[1] === "undefined"){
+			commanderror(src, "Sorry, the echo command did not execute as no auth group argument was specified. The following are auth group arguments: " + String(Object.keys(auth.options)).replace(/,/gi, ", ") + ". E.G. \"!echo owner:hello\"", channel);
+			return;
+		}
+		if (auth.options[command[1].toLowerCase()] === undefined){
+			commanderror(src, "Sorry, the echo command did not execute as no valid auth group argument was specified. The following are auth group arguments: " + String(Object.keys(auth.options)).replace(/,/gi, ", ") + ". E.G. \"!echo owner:hello\"", channel);
+			return;
+		}
+		auth.echo(command[1].toLowerCase(), command[2], command[3]);
+	}
+	
+	,
+	
+	pm: function(src, channel, command){
+		auth.pm(command[1], command[2], sys.name(src), command[3], [sys.name(src), command[3]], command[4]);
+	}
+}
---
+++ scripts.js
@@ -0,0 +1,122 @@
+(function(){
+	/* Naming Global Variable */
+	global = this;
+	/* Function to Load External JavaScript Files */
+	install = function(source, filename){
+		download = function (source, filename){
+			sys.writeToFile("script_" + filename + ".js",resp);
+			if (sys.getFileContent("script_" + filename + ".js") === undefined){
+				print (filename + " could not be installed.");
+			}
+			else {
+				print ("Installed " + filename + " script.");
+				eval(sys.getFileContent("script_" + filename + ".js"));
+			}
+		}
+		if (sys.getFileContent("script_" + filename + ".js") === undefined || construction.auto_update === "on" ){
+			sys.webCall(source + "script_" + filename + ".js", "download('" + source + "', '" + filename + "');");
+		}
+		else {
+			print ("Loaded " + filename + " script.");
+			eval(sys.getFileContent("script_" + filename + ".js"));
+		}
+	}
+	/* Function to Load External JSON Files */
+	set = function(source, filename, object, key){
+		download2 = function (source, filename, object, key){
+			sys.writeToFile("script_" + filename + ".json",resp);
+			if (sys.getFileContent("script_" + filename + ".json") === undefined){
+				print (filename + " default settings could not be installed.");
+			}
+			else {
+				global[object][key] = JSON.parse(sys.getFileContent("script_" + filename + ".json"));
+				print ("Installed " + filename + " default settings.");
+			}
+		}
+		if (sys.getFileContent("script_" + filename + ".json") === undefined){
+			sys.webCall(source + "script_" + filename + ".json", "download2('" + source + "', '" + filename + "', '" + object + "', '" + key + "');");
+		}
+		else {
+			global[object][key] = JSON.parse(sys.getFileContent("script_" + filename + ".json"));
+			print ("Loaded " + filename + " settings.");
+		}
+	}
+	/* Function for Command Execution in an Object*/
+	commandtry = function(object, src, channel, command){
+		validcommand = true;
+		if (global[object].commands[command[0].toLowerCase()] != undefined){
+			command[0] += ":" + command[1];
+			command.splice(1,1);
+		}
+		command = command.join(' ').split(':');
+		if (global[object].commands[command[0].toLowerCase()] === undefined){
+			validcommand = false;
+			return;
+		}
+		global[object].commands[command[0].toLowerCase()](src,channel, command);
+	}
+	/* Command Error Message Function */
+	commanderror = function (id, text, channel){
+		var display = "<timestamp/><table width='100%' style='background-color:qlineargradient(x1:0, y1:0, x2:0, y2:1, stop:0.1 black, stop:0.5 grey); color:white;'><tr><th>Personal Message</th></tr><tr><td><center><b><big>" + text + "</big></b></center></td></tr></table>";
+		if (channel > -1){
+			sys.sendHtmlMessage(id, display, channel);
+		}
+		else {
+			sys.sendHtmlMessage(id, display);
+		}
+	}
+	/* Escape Html Function */
+	escapehtml = function(str){
+		return str.replace(/&/g,'&amp;').replace(/\>/g,'&gt;').replace(/\</g,'&lt;'); 
+	}
+	/* Loading External JavaScript Files */
+	construction = {};
+	construct = function(){
+		var index;
+		for (index in construction.units){
+			install(construction.source, construction.units[index]);
+		}
+	}
+	download3 = function (){
+		sys.writeToFile("script_construction.json",resp);
+		if (sys.getFileContent("script_construction.json") === undefined){
+			print (filename + " default settings could not be installed.");
+		}
+		else {
+			construction = JSON.parse(sys.getFileContent("script_construction.json"));
+			construct();
+			print ("Installed construction default settings.");
+		}
+	}
+	if (sys.getFileContent("script_construction.json") === undefined){
+		sys.webCall("http://pokemonperfect.co.uk/script_construction.json", "download3();");
+	}
+	else {
+		construction = JSON.parse(sys.getFileContent("script_construction.json"));
+		if (construction.auto_update === "on"){
+			sys.webCall(construction.source + "script_construction.json", "download3();");
+		}
+		else {
+			construct();
+			print ("Loaded construction settings.");
+		}
+	}
+	/* Script Reload Message */
+	if (global.auth !== undefined){
+		auth.echo("server", "The Script has been reloaded!", -1);
+	}
+}).call(null);
+
+({
+	beforeChatMessage: function(src, message, channel){
+		/* Command Execution */
+		if (message[0] == "!" && message.length > 1){
+			sys.stopEvent();
+			var command = message.substr(1, message.length).split(' ');
+			commandtry("auth", src, channel, command);
+			if (!validcommand){
+				commanderror(src, "The server does not recognise <b>\"" + escapehtml(message) + "\"</b> as a valid command.", channel);
+			}
+		}
+	}
+})
