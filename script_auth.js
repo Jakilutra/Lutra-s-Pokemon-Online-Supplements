24b97ab03b56a7e1ebef2f85f491cc0123356c7b
Jakilutra <jakilutra@hotmail.co.uk>
2012-07-13 02:32:23 +0100

    Improved /echo.

--- script_auth.js
+++ script_auth.js
@@ -4,7 +4,7 @@ set(construction.source, "authoptions", "auth", "options");
 set(construction.source, "authmembers", "auth", "members");
 /* Auth-based Announcement Function */
 auth.echo = function(group, text, channel){
-	var display = auth.options === undefined ? "<timestamp/><b>" + text + "</b>" : "<timestamp/><table width='100%' style='background-color:qlineargradient(x1:0, y1:0, x2:0, y2:1, stop:0.1 " + auth.options[group].minorcolor + ", stop:0.5 " + auth.options[group].majorcolor + "); color:" + auth.options[group].textcolor + ";'><tr><td><center><b><big>" + text + "</big></b></center></td></tr></table>";
+	var display = "<timestamp/><table width='100%' style='background-color:qlineargradient(x1:0, y1:0, x2:0, y2:1, stop:0.1 " + auth.options[group].minorcolor + ", stop:0.5 " + auth.options[group].majorcolor + "); color:" + auth.options[group].textcolor + ";'><tr><td><center><b><big>" + text + "</big></b></center></td></tr></table>";
 	if (channel > -1){
 		sys.sendHtmlAll(display, channel);
 	}
@@ -14,7 +14,7 @@ auth.echo = function(group, text, channel){
 }
 /* Main Chat PM Function */
 auth.pm = function(group, text, from, to, recipients, channel){
-	var display = auth.options === undefined ? "<timestamp/> from " + from + " to" + to + " Sent to: " + String(recipients).replace(/,/gi, ", ") + "<br/><b>" + text + "</b>" : "<timestamp/><table width='100%' style='background-color:qlineargradient(x1:0, y1:0, x2:0, y2:1, stop:0.1 " + auth.options[group].minorcolor + ", stop:0.5 " + auth.options[group].majorcolor + "); color:" + auth.options[group].textcolor + ";'><tr><th>Personal Message from <i>" + from + "</i> to <i>" + to + "</i>&nbsp;&nbsp;&nbsp;Sent to: <i>" + String(recipients).replace(/,/gi, ", ") + "</i></th></tr><tr><td><center><b><big>" + text + "</big></b></center></td></tr></table>";
+	var display = "<timestamp/><table width='100%' style='background-color:qlineargradient(x1:0, y1:0, x2:0, y2:1, stop:0.1 " + auth.options[group].minorcolor + ", stop:0.5 " + auth.options[group].majorcolor + "); color:" + auth.options[group].textcolor + ";'><tr><th>Personal Message from <i>" + from + "</i> to <i>" + to + "</i>&nbsp;&nbsp;&nbsp;Sent to: <i>" + String(recipients).replace(/,/gi, ", ") + "</i></th></tr><tr><td><center><b><big>" + text + "</big></b></center></td></tr></table>";
 	var index;
 	if (channel > -1){
 		for (index in recipients){
@@ -30,10 +30,10 @@ auth.pm = function(group, text, from, to, recipients, channel){
 /* Auth Commands */
 auth.commands = {
 	authcommands:function(src, channel, command){
-		var osymbol = auth.options["owner"].image, usymbol = auth.options["user"].image;
+		var osymbol = auth.options["owner"].image, msymbol = auth.options["mod"].image, usymbol = auth.options["user"].image;
 		var display = typecommands
-		+ "<tr><td><center>" + osymbol + "<font color='darkgreen'><b>/echo</b></font><font color='red'><b> authgroup</b></font><font color='blue'><b>*message</b></font>: displays <b>message</b> with the announcement background of <b>authgroup.</b> </center></td></tr>"
-		+ "<tr><td><center>" + usymbol + "<font color='darkgreen'><b>/authranks</b></font>: displays the auth groups and symbols.</center></td></tr>";
+		+ "<tr><td><center>" + msymbol + "<b><font color='darkgreen'>/echo</font><font color='red'> authgroup</font><font color='blue'>*message</font><font color='darkviolet'>*channel</font></b>: displays <b>message</b> with the announcement background of <b>authgroup</b> - in <b>channel</b> if a name of a channel is specified. </center></td></tr>"
+		+ "<tr><td><center>" + usymbol + "<b><font color='darkgreen'>/authranks</font></b>: displays the auth groups and symbols.</center></td></tr>";
 		commanddisplay(src, "Auth Commands", display, channel);		
 	}
 	,
@@ -47,20 +47,28 @@ auth.commands = {
 	}
 	,
 	echo: function(src, channel, command){
-		if (sys.auth(src) < 3){
-			commanderror(src, "Sorry, you do not have permission to use the echo command (owner command).", channel);
+		if (sys.auth(src) < 1){
+			commanderror(src, "Sorry, you do not have permission to use the echo command (mod command).", channel);
 			return;
+		}
+		var index, authgroup, authnames = [];
+		for (index in auth.options){
+			var name = auth.options[index].name.toLowerCase();
+			authnames.push(name);
+			if (name === command[1].toLowerCase()){
+				authgroup = index;
+			}
 		}
 		if (command[1] === "undefined"){
-			commanderror(src, "Sorry, the echo command did not execute as no auth group argument was specified. The following are auth group arguments: " + String(Object.keys(auth.options)).replace(/,/gi, ", ") + ". E.G. \"/echo owner*hello\"", channel);
+			commanderror(src, "Sorry, the echo command did not execute as no auth group argument was specified. The following are auth group arguments: " + String(authnames).replace(/,/gi, ", ") + ".<br/> E.G. \"/echo owner*hello\"", channel);
 			return;
 		}
-		if (auth.options[command[1].toLowerCase()] === undefined){
-			commanderror(src, "Sorry, the echo command did not execute as no valid auth group argument was specified. The following are auth group arguments: " + String(Object.keys(auth.options)).replace(/,/gi, ", ") + ". E.G. \"/echo owner*hello\"", channel);
+		if (authgroup === undefined){
+			commanderror(src, "Sorry, the echo command did not execute as no valid auth group argument was specified. The following are auth group arguments: " + String(authnames).replace(/,/gi, ", ") + ".<br/> E.G. \"/echo owner*hello\"", channel);
 			return;
 		}
 		sys.sendAll(sys.name(src) + ":");
-		auth.echo(command[1].toLowerCase(), command[2], command[3]);
+		auth.echo(authgroup, command[2], sys.channelId(command[3]));
 	}
 	,	
 	pm: function(src, channel, command){
