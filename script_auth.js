0ab0fcaabedf99dac462ad020b9bc1be1e1e15e3
Jonah Stanley <swimming95@aol.com>
2012-08-20 17:08:41 -0400

    Beginning of PM command

--- script_auth.js
+++ script_auth.js
@@ -4,6 +4,9 @@ set(construction.source, "authoptions", "auth", "options");
 set(construction.source, "authmembers", "auth", "members");
 
 /* Creating PMs Object */
+
+/* pm object will contain purely a name and an array of from,recipients,messages to that name.
+Upon login, a person's pms will be shown and thus deleted from this object*/
 if (auth.pms === undefined){
 	auth.pms = {};
 }
@@ -24,14 +27,27 @@ auth.pm = function (group, text, from, to, recipients, channel) {
 	var display = "<timestamp/><table width='100%' style='background-color:qlineargradient(x1:0, y1:0, x2:0, y2:1, stop:0.1 " + auth.options[group].minorcolor + ", stop:0.5 " + auth.options[group].majorcolor + "); color:" + auth.options[group].textcolor + ";'><tr><th>Personal Message from <i>" + from + "</i> to <i>" + to + "</i>&nbsp;&nbsp;&nbsp;Sent to: <i>" + String(recipients).replace(/,/gi, ", ") + "</i></th></tr><tr><td><center><b><big>" + text + "</big></b></center></td></tr></table>";
 	var index;
 	if (channel > -1) {
-		for (index in recipients) {
-			sys.sendHtmlMessage(sys.id(recipients[index]), display, channel);
-		}
+			sys.sendHtmlMessage(to, display, channel);
 	}
 	else {
-		for (index in recipients) {
-			sys.sendHtmlMessage(sys.id(recipients[index]), display);
-		}
+			sys.sendHtmlMessage(to, display);
+	}
+}
+
+/* Finding correct group*/
+auth.groupName=function(name){
+	var authLevel=sys.auth(name);
+	if(authLevel===3){
+	return "owner"
+	}
+	else if(authLevel===2){
+	return "admin"
+	}
+	else if(authLevel===1){
+	return "mod"
+	}
+	else{
+	return "user"
 	}
 }
 
@@ -86,6 +102,56 @@ auth.commands = {
 		auth.echo(authgroup, command, channelid);
 	},
 	pm: function (src, channel, command) {
-		auth.pm(command[1], command[2], sys.name(src), command[3], [sys.name(src), command[3]], command[4]);
+		if(command.length <3){
+			sys.sendHtmlMessage(src, "PM command is used with the following Arguments: TO*MESSAGE*CC1*CC2...",channel);
+		}
+		var sendTo=[command[1]];
+		if(command.length>3){
+			sentTo=sentTo.concat(command.slice(3));
+		}
+		var message = command[2];
+		//ability to make maxmessagelength here
+		var failure=[]
+		for(var i=0;i<sendTo.length;i++){
+			//user doesn't exist
+			if(members[sendTo[i].toLowerCase()] === undefined){
+				failure.push(sendTo[i]);
+			}
+			//user is offline
+			else if(sys.id(sendTo[i])===undefined){
+				if(auth.pms[sendTo[i]]!=undefined){
+					//potentially check for max mailbox size
+					auth.pms[sendTo[i].toLowerCase()].push({
+						"from":sys.name(src),
+						"message":message,
+						"recipients":sendTo
+					})
+				}
+				else{
+					auth.pms[sendTo[i].toLowerCase()]=[{
+						"from":sys.name(src),
+						"message":message,
+						"recipients":sendTo
+					}];
+				}
+			}
+			//user is online
+			else{
+				auth.pm(auth.groupName(sys.name(src)),message,sys.name(src),command[1],sendTo,channel)
+			}
+		}
+		if(failure.length !==0){
+			sys.sendHtmlMessage(src,"The message failed to send to the following users: "+failure+".  Please check the spelling of all of the names",channel);
+		}
 	}
 }
+
+
+append("afterLogIn",
+"\t\tvar lowerName=sys.name(src).toLowerCase() " +
+"\r\t\tif(auth.pms[lowerName]!=undefined){"+
+"\r\t\t\tfor(var i=0;i<auth.pms[lowerName].length;i++){"+
+"\r\t\t\t\tauth.pm(auth.groupName(auth.pms[lowerName][\"from\"]),auth.pms[lowerName][\"message\"],auth.pms[lowerName][\"from\"],sys.name(src),auth.pms[lowerName][\"recipients\"]);"+
+"\r\t\t\t}"+
+"\r\t\t\tdelete auth.pms[lowerName]"+
+"\r\t\t\t}")
